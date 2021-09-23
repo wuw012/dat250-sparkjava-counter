68c16e5700f4728126cedd0202f60573f24160ce
Lars Kristensen <larskristensen@158-37-71-18.hvl.no>
2021-09-23 11:23:57 +0200

    added GET services for individual counters

--- counters/counters/src/main/java/no/hvl/dat110/rest/counters/App.java
+++ counters/counters/src/main/java/no/hvl/dat110/rest/counters/App.java
@@ -32,7 +32,13 @@ public class App {
 		get("/hello", (req, res) -> "Hello World!");
 		
         get("/counters", (req, res) -> counters.toJson());
-               
+ 
+        get("/counters/red", (req, res) -> counters.getRed());
+
+        get("/counters/green", (req, res) -> counters.getGreen());
+
+        // TODO: put for green/red and in JSON
+        // variant that returns link/references to red and green counter
         put("/counters", (req,res) -> {
         
         	Gson gson = new Gson();
